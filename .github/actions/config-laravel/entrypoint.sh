161b2d4b0d055a54d8640613fc165f065979e342
wolneycarneiro91 <wolneycarneiro91@gmail.com>
2022-01-06 17:07:34 -0300

    Adicionando configuração laravel

--- .github/actions/config-laravel/entrypoint.sh
+++ .github/actions/config-laravel/entrypoint.sh
@@ -1,10 +1,4 @@
 #!bin/sh
-# cp .env.example .env
 php artisan --version
-# php artisan cache:clear
-# php artisan config:clear
-# php artisan key:generate
-# php artisan migrate
-# php artisan db:seed
 
 
