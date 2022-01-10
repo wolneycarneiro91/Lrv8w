6dbe4e273f767e9963149c462363133ae463ab06
wolneycarneiro91 <wolneycarneiro91@gmail.com>
2022-01-10 01:26:40 -0300

    sail up

--- .github/actions/laravel_build/entrypoint.sh
+++ .github/actions/laravel_build/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-#curl -s "https://laravel.build/judge-app?with=pgsql,redis" | bash
-cd  _work/Lrv8w/Lrv8w/judge-app
+curl -s "https://laravel.build/app-runned?with=pgsql,redis" | bash
+cd  app-runned
  ./vendor/bin/sail up
 #php artisan --version
 
